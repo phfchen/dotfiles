a4e5451b564a1fbd85a104dd5c2b96bcd88fcef3
phfchen <fchen@necindustries.com>
2026-06-07 15:15:55 -0700

    Updating script files

--- scripts/updatewallpapers.sh
+++ scripts/updatewallpapers.sh
@@ -2,6 +2,7 @@
 WALLPAPERSDIR="$HOME/Pictures/Wallpapers"
 GITWALLPAPERS="$HOME/Documents/git/phfchen/wallpapers/"
 
+mkdir -p "$WALLPAPERSDIR"
 rm -f "$WALLPAPERSDIR"/*
 cd "$GITWALLPAPERS"
 find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.webp" \) -exec ln -s "$(realpath {})" "$WALLPAPERSDIR"/ \;
